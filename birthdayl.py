42d973852e0d22d7bf72f0e97807a030e0b7606e
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-25 00:10:17 +0300

    hbd lind

---
+++ birthdayl.py
@@ -0,0 +1,29 @@
+#Request userr for name 
+#initialize necessary variables
+#Request for answer the joke game 
+#if yes,state joke and request for answer
+#if correct ,applause if not 
+#End activity
+name=str(input("Enter your name"))
+p="yes" or "no"
+answer=str(input("Enter the answer "))
+answer='shellebrates' or 'peanuts'
+
+music="Pop Music"  or "AfroBeat"
+if name=="Linda":
+   
+    print("This is a birthday game,would you like to coninue?")
+
+if p=="yes":
+    print("What does a clam do on its birthday ")
+
+if answer=="shellebrates":
+    print("Great!")
+else:
+    print("Answer is shellebrates!")
+
+print("What kind of music is scary for birthday ballons")
+
+
+
+
